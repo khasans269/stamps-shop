15f95b877075dbec6730f6a8fe27914283acf90b
Askar <askarsibay@gmail.com>
2026-07-08 11:50:02 +0300

    chore(metrika): подключить счётчик Яндекс.Метрики через env

--- components/analytics/YandexMetrika.tsx
+++ components/analytics/YandexMetrika.tsx
@@ -29,12 +29,14 @@ export function YandexMetrika() {
               if (document.scripts[j].src === r) { return; }
             }
             k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-          })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+          })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js?id=${METRIKA_ID}", "ym");
           ym(${METRIKA_ID}, "init", {
+            ssr: true,
+            webvisor: true,
             clickmap: true,
-            trackLinks: true,
+            ecommerce: "dataLayer",
             accurateTrackBounce: true,
-            webvisor: true
+            trackLinks: true
           });
         `}
       </Script>
